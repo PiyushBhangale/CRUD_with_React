5309a876a5e275ea09b07db2375a0d10b4df9d3f
PiyushBhangale <piyushbhangale1995@gmail.com>
2018-11-29 20:29:45 +0530

    Updates

---
+++ precache-manifest.68076adbf79b1fa9f578c6e7edc7a61b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3cd4b2bef48b29472d1",
+    "url": "/CRUD_with_React/static/js/runtime~main.f3cd4b2b.js"
+  },
+  {
+    "revision": "3959ad5448eab7179325",
+    "url": "/CRUD_with_React/static/js/main.3959ad54.chunk.js"
+  },
+  {
+    "revision": "ac0dc92fbb115a0206f4",
+    "url": "/CRUD_with_React/static/js/1.ac0dc92f.chunk.js"
+  },
+  {
+    "revision": "3959ad5448eab7179325",
+    "url": "/CRUD_with_React/static/css/main.7e73268b.chunk.css"
+  },
+  {
+    "revision": "8af25e39a8d4b5a4a80f523499fb5554",
+    "url": "/CRUD_with_React/index.html"
+  }
+];
